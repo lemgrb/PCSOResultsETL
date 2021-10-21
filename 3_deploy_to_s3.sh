afd0acd81ca89a0679eea6d21117aca58af745e2
Lemuel Bulangis <lemgrb@gmail.com>
2021-10-21 12:21:31 +0800

    Update shell script #3

--- 3_deploy_to_s3.sh
+++ 3_deploy_to_s3.sh
@@ -8,11 +8,11 @@ cp -r ../plus63-frontend/out/resulthistory s3
 cp ../plus63-frontend/out/sitemap.xml s3
 cp ../plus63-frontend/out/index.html s3
 echo "Delete old files"
-aws s3 rm $S3URI/_next --recursive
-aws s3 rm $S3URI/resulthistory --recursive
-aws s3 rm $S3URI/sitemap.xml --recursive
-aws s3 rm $S3URI/index.html --recursive
+aws s3 rm s3://$S3URI/_next --recursive
+aws s3 rm s3://$S3URI/resulthistory --recursive
+aws s3 rm s3://$S3URI/sitemap.xml --recursive
+aws s3 rm s3://$S3URI/index.html --recursive
 echo "Sync to s3"
-aws s3 sync s3 $S3URI
+aws s3 sync s3 s3://$S3URI
 echo "Invalidating cache"
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION --paths "/*"
